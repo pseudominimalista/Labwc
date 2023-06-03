938bf27811d69603a48020c390c52fde9f87cad7
pseudo-minimalista <pseudo-minimalista@noreply.codeberg.org>
2023-06-03 15:47:46 +0000

    Atualizar 'instalar.sh'

--- instalar.sh
+++ instalar.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-.local/bin/yay -Syu git vim vim-plug nordic-darker-theme nordic-wallpapers-git wbg labwc foot wf-recorder wl-clipboard brightnessctl pavucontrol thunar grim imv mako mpv xdg-utils nordzy-cursors ttf-jetbrains-mono acpi xdg-desktop-portal-wlr rofi xorg-xwayland yt-dlp
+.local/bin/yay -Syu git vim-nord google-chrome nordic-darker-theme nordic-wallpapers-git wbg labwc foot wf-recorder wl-clipboard brightnessctl pavucontrol thunar grim imv mako mpv xdg-utils nordzy-cursors ttf-jetbrains-mono acpi xdg-desktop-portal-wlr rofi xorg-xwayland yt-dlp
 Nordzy-icon/install.sh
 
 xdg-user-dirs-update --force
-cp -r .vim ~/
 cp -r .config ~/
 cp -r .local ~/
-cp .vimrc ~/
 cp .bash* ~/
