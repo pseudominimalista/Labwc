697420cf1cecba9e444e4e551f69f358eb8a58a8
pseudominimalista <github@pseudominimalista.com>
2023-06-03 12:39:44 -0300

    FINALMENTE UM UPDATE

--- instalar.sh
+++ instalar.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+<<<<<<< HEAD
 .local/bin/yay -Syu git vim vim-plug nxengine nordic-darker-theme nordic-wallpapers-git wbg labwc foot librewolf-bin wf-recorder wl-clipboard wdisplays brightnessctl pavucontrol thunar tumbler grim imv mako mpv xdg-user-dirs xdg-utils nordzy-cursors ttf-jetbrains-mono acpi xdg-desktop-portal-wlr rofi xorg-xwayland yt-dlp
+=======
+.local/bin/yay -Syu git vim vim-plug nordic-darker-theme nordic-wallpapers-git wbg labwc foot wf-recorder wl-clipboard brightnessctl pavucontrol thunar grim imv mako mpv xdg-utils nordzy-cursors ttf-jetbrains-mono acpi xdg-desktop-portal-wlr rofi xorg-xwayland yt-dlp
+>>>>>>> bdbeb62 (FINALMENTE UM UPDATE)
 Nordzy-icon/install.sh
 
 xdg-user-dirs-update --force
