800c968fa9c1365a862547fa5b53a37707ec7fa7
John Doe <120287606+pseudominimalista@users.noreply.github.com>
2023-07-02 13:55:32 +0000

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-.local/bin/yay -Syu git google-chrome nordic-darker-theme nordzy-icon-theme nordic-wallpapers-git \
+.local/bin/yay -Syu git librewolf-bin nordic-darker-theme nordzy-icon-theme nordic-wallpapers-git \
 wbg labwc foot wf-recorder wl-clipboard brightnessctl pavucontrol thunar gvfs gvfs-mtp gvfs-smb \
 grim imv mako mpv xdg-utils nordzy-cursors ttf-jetbrains-mono acpi xdg-desktop-portal-wlr rofi \
 xorg-xwayland yt-dlp blueman libreoffice mupdf file-roller p7zip wine nwg-look-bin \
-playerctl swaylock lite-xl-bin
+playerctl swaylock lite-xl-bin alsa-utils
 
 xdg-user-dirs-update --force
 cp -r .config ~/
