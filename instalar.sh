da2c9c20ec21ab52ae01935f4355d568b35ed6cc
John Doe <120287606+pseudominimalista@users.noreply.github.com>
2023-07-06 22:48:54 +0000

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 .local/bin/yay -Syu git librewolf-bin nordic-darker-theme nordzy-icon-theme nordic-wallpapers-git \
 wbg labwc foot wf-recorder wl-clipboard brightnessctl pavucontrol thunar gvfs gvfs-mtp gvfs-smb \
-grim imv mako mpv xdg-utils nordzy-cursors ttf-jetbrains-mono acpi xdg-desktop-portal-wlr rofi \
+grimshot imv mako mpv xdg-utils nordzy-cursors ttf-jetbrains-mono acpi xdg-desktop-portal-wlr rofi \
 xorg-xwayland yt-dlp blueman libreoffice mupdf file-roller p7zip wine nwg-look-bin \
 playerctl swaylock lite-xl-bin alsa-utils xdg-utils xdg-user-dirs
 
