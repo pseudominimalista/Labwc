a264b935fbbf1edc669df89f264bfc4e42dba83b
Diogo Fernandes <120287606+pseudominimalista@users.noreply.github.com>
2023-07-14 18:50:10 +0000

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 yay -Syu librewolf-bin whitesur-gtk-theme whitesur-icon-theme whitesur-cursor-theme-git \
 wbg labwc foot wl-clipboard brightnessctl pavucontrol thunar gvfs gvfs-mtp gvfs-smb \
-grimshot imv mako mpv ttf-jetbrains-mono xdg-desktop-portal-wlr rofi \
-xorg-xwayland yt-dlp libreoffice-fresh mupdf file-roller p7zip wine \
-playerctl swaylock lite-xl-bin alsa-utils xdg-utils xdg-user-dirs tumbler
+grimshot imv mako mpv ttf-jetbrains-mono xdg-desktop-portal-wlr rofi xorg-xwayland \
+yt-dlp mupdf file-roller p7zip wine playerctl swaylock lite-xl-bin alsa-utils \
+xdg-utils xdg-user-dirs tumbler
 
 xdg-user-dirs-update --force
 
